60b52229cba7730aface465924ef7b3587a06bf7
bartula <bartlomiej.rybski@gmail.com>
2022-11-15 17:31:08 +0100

    XPath training.

---
+++ Selenium-Udemy/XPath training.cs
@@ -0,0 +1,33 @@
+﻿using AngleSharp.Dom;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace Selenium_Udemy
+{
+    internal class XPath_training
+    {
+        IWebDriver driver;
+        String url = "add proper link here";
+
+        [SetUp]
+        public void OpeningBrowser()
+        {
+            new WebDriverManager.DriverManager().SetUpDriver(new ChromeConfig()); //WebDriverManager package that helps implementing proper chromedriver for currently installed browser version
+            driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Navigate().GoToUrl(url);
+        }
+
+        [TearDown]
+        public void QuitBrowser()
+        {
+            driver.Quit();
+        }
+    }
+}
