b4e848f9271547913a6debe6832888bfac13fe16
bartula <bartlomiej.rybski@gmail.com>
2022-11-24 23:14:43 +0100

    Switching between windows methods.

--- Selenium-Udemy/WindowHandling.cs
+++ Selenium-Udemy/WindowHandling.cs
@@ -32,11 +32,26 @@ namespace Selenium_Udemy
         [Test]
         public void WindowHandle()
         {
+            String parentWindowId = driver.CurrentWindowHandle;
+            
             //click the link which open new tab in browser
             driver.FindElement(By.XPath("//a[contains(text(),'Free Access')]")).Click();
             
             //assert with property WindowHandles.Count that there are 2 pages opened;
-            //Assert.That(driver.WindowHandles.Count, Is.EqualTo(2));
+            Assert.That(driver.WindowHandles.Count, Is.EqualTo(2));
+
+            //switch to second tab and then gather email address which is shown on red (part of the string) and try to log in with that email
+            driver.SwitchTo().Window(driver.WindowHandles[1]);
+            string txt = driver.FindElement(By.XPath("//p[@class='im-para red']")).Text;
+            String[] splittedText = txt.Split("at");
+            String[] trimmedString = splittedText[1].Trim().Split(" ");
+            driver.SwitchTo().Window(parentWindowId);
+            driver.FindElement(By.Id("username")).SendKeys(trimmedString[0]);
+
+            //assert that data inserted into login is equal to expected email
+            String email = driver.FindElement(By.Id("username")).GetAttribute("value");
+            Assert.That(email, Is.EqualTo("mentor@rahulshettyacademy.com"));
+            
 
         }
     }
